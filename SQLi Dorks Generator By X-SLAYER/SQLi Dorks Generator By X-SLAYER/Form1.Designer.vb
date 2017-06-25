2550a006954281d1198b4678f84db43b1fa11673
Virgula0 <angelo.rosa1999@gmail.com>
2017-06-25 13:43:54 +0200

    Add files via upload

---
+++ SQLi Dorks Generator By X-SLAYER/SQLi Dorks Generator By X-SLAYER/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pg3 = New System.Windows.Forms.TextBox()
+        Me.pg2 = New System.Windows.Forms.TextBox()
+        Me.Page_Names = New System.Windows.Forms.TextBox()
+        Me.replacer = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.txtres = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(599, 465)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.pg3)
+        Me.TabPage1.Controls.Add(Me.pg2)
+        Me.TabPage1.Controls.Add(Me.Page_Names)
+        Me.TabPage1.Controls.Add(Me.replacer)
+        Me.TabPage1.ForeColor = System.Drawing.Color.White
+        Me.TabPage1.ImageIndex = 0
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(591, 439)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Main"
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(7, 406)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(578, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Generat Dorks"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(437, 17)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(148, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Page Types"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(268, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Page Format"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(237, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Page Names"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'pg3
+        '
+        Me.pg3.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.pg3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pg3.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pg3.ForeColor = System.Drawing.Color.White
+        Me.pg3.Location = New System.Drawing.Point(437, 37)
+        Me.pg3.Multiline = True
+        Me.pg3.Name = "pg3"
+        Me.pg3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.pg3.Size = New System.Drawing.Size(148, 363)
+        Me.pg3.TabIndex = 3
+        Me.pg3.Text = "id=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "category=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Pageid=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "category=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ID=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "decl_id=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "pageid=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "file=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "staff_id=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "num" & _
+    "=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "cat=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "avd=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ogl_id="
+        '
+        'pg2
+        '
+        Me.pg2.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.pg2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pg2.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pg2.ForeColor = System.Drawing.Color.White
+        Me.pg2.Location = New System.Drawing.Point(268, 37)
+        Me.pg2.Multiline = True
+        Me.pg2.Name = "pg2"
+        Me.pg2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.pg2.Size = New System.Drawing.Size(148, 363)
+        Me.pg2.TabIndex = 2
+        Me.pg2.Text = ".cfm?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".php?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".asp?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".html?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".jsp?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".aspx?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".cgi?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".flv?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".pdf?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".jsf?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".jsp?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".a" & _
+    "shx?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".psml?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".raw?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".File?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".tss?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".blog?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & ".htm?"
+        '
+        'Page_Names
+        '
+        Me.Page_Names.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.Page_Names.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Page_Names.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Page_Names.ForeColor = System.Drawing.Color.White
+        Me.Page_Names.Location = New System.Drawing.Point(7, 37)
+        Me.Page_Names.Multiline = True
+        Me.Page_Names.Name = "Page_Names"
+        Me.Page_Names.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Page_Names.Size = New System.Drawing.Size(238, 363)
+        Me.Page_Names.TabIndex = 1
+        '
+        'replacer
+        '
+        Me.replacer.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.replacer.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.replacer.ForeColor = System.Drawing.Color.White
+        Me.replacer.Location = New System.Drawing.Point(8, 37)
+        Me.replacer.Multiline = True
+        Me.replacer.Name = "replacer"
+        Me.replacer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.replacer.Size = New System.Drawing.Size(238, 363)
+        Me.replacer.TabIndex = 12
+        Me.replacer.Visible = False
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.TabPage2.Controls.Add(Me.txtres)
+        Me.TabPage2.ImageIndex = 1
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(591, 439)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Results"
+        '
+        'txtres
+        '
+        Me.txtres.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.txtres.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtres.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtres.ForeColor = System.Drawing.Color.White
+        Me.txtres.Location = New System.Drawing.Point(3, 3)
+        Me.txtres.Multiline = True
+        Me.txtres.Name = "txtres"
+        Me.txtres.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtres.Size = New System.Drawing.Size(585, 433)
+        Me.txtres.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(599, 465)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SQLi Dorks Generator | By X-SLAYER"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents txtres As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents pg3 As System.Windows.Forms.TextBox
+    Friend WithEvents pg2 As System.Windows.Forms.TextBox
+    Friend WithEvents Page_Names As System.Windows.Forms.TextBox
+    Friend WithEvents replacer As System.Windows.Forms.TextBox
+
+End Class
---
+++ SQLi Dorks Generator By X-SLAYER/SQLi Dorks Generator By X-SLAYER/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.IO
+Imports System.Threading
+
+
+Public Class Form1
+
+    'BY X-SLAYER
+    'Youtube : https://www.youtube.com/channel/UC-lOXlEl67lxmYTH2PTFovA
+
+    Public Sub New()
+        InitializeComponent()
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim TT As New Thread(AddressOf Generator)
+        TT.Start()
+    End Sub
+
+    Public Sub Generator()
+        If Not Directory.Exists((Application.StartupPath & "\Results\")) Then
+            Directory.CreateDirectory((Application.StartupPath & "\Results\"))
+        End If
+        Dim str As String = Strings.Format(DateAndTime.Now, "dd.MM.%y-HH.mm.ss")
+        Dim path As String = (Application.StartupPath & "\Results\Dorks-" & str & ".txt")
+        If Not File.Exists(path) Then
+            Using writer As StreamWriter = File.CreateText(path)
+                Dim str4 As String
+                For Each str4 In Me.Page_Names.Lines
+                    Dim str5 As String
+                    For Each str5 In Me.pg2.Lines
+                        Dim str6 As String
+                        For Each str6 In Me.pg3.Lines
+                            writer.WriteLine((str4 & str5 & str6))
+                        Next
+                    Next
+                Next
+                writer.Close()
+                txtres.Text = String.Empty
+                Thread.Sleep(25)
+                txtres.Text = File.ReadAllText(path)
+                MsgBox("Dorks Generated Succesfully", MsgBoxStyle.Information)
+                If MessageBox.Show("Do You Want To Open Resultat Folder", "Message", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                    Process.Start(Application.StartupPath & "\Results\", AppWinStyle.NormalFocus)
+                End If
+            End Using
+        End If
+    End Sub
+End Class
